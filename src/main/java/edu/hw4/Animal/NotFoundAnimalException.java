594a28bced0a26e23ef3a94460975ea36be05cf4
Ann Chernova <ann1974chernova@gmail.com>
2023-10-31 02:51:40 +0300

    fix hw4

--- src/main/java/edu/hw4/Animal/NotFoundAnimalException.java
+++ src/main/java/edu/hw4/Animal/NotFoundAnimalException.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public class NotFoundAnimalException extends Exception {
     @Override
     public String getMessage() {
-        log.info("No animal with these parameters in the sheet");
+        log.info("No animal with these parameters in the list");
         return super.getMessage();
     }
 }
