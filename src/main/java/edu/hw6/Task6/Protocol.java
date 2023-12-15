fa98b96431754957d939d6ba5b374e84bf183f68
Ann Chernova <ann1974chernova@gmail.com>
2023-12-16 01:40:28 +0300

    update hw10

--- src/main/java/edu/hw6/Task6/Protocol.java
+++ src/main/java/edu/hw6/Task6/Protocol.java
@@ -3,6 +3,7 @@ package edu.hw6.Task6;
 public enum Protocol {
     UDP("UDP"), TCP("TCP"), UDP_TCP("UDP/TCP");
     private final String protocolName;
+
     Protocol(String protocolName) {
         this.protocolName = protocolName;
     }
